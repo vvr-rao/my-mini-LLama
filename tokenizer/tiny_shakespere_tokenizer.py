24d676748e8959d104ef9ad91afd97419105bcca
vvr-rao <38475577+vvr-rao@users.noreply.github.com>
2024-05-25 06:17:06 -0400

    Update tiny_shakespere_tokenizer.py

--- tokenizer/tiny_shakespere_tokenizer.py
+++ tokenizer/tiny_shakespere_tokenizer.py
@@ -25,9 +25,21 @@ class SimpleBPETokenizer:
 
 def get_stats(ids):
     counts = {}
-    for pair in zip(ids, ids[1:]): # Pythonic way to iterate consecutive elements
+    for pair in zip(ids, ids[1:]):
         counts[pair] = counts.get(pair, 0) + 1
     return counts
+
+def merge(ids, pair, idx):
+  newids = []
+  i = 0
+  while i < len(ids):
+    if i < len(ids) - 1 and ids[i] == pair[0] and ids[i+1] == pair[1]:
+      newids.append(idx)
+      i += 2
+    else:
+      newids.append(ids[i])
+      i += 1
+  return newids
 
 def get_tokenizer():
     file_name = f'./vocab/merges.pkl'
